0a97abdb1789c55e17aa18fd5d4fee71049520b8
Ryo Nakagome <nakagome@tento-net.com>
2018-03-20 02:17:44 +0900

    バグだらけのプログラムを生成した。

--- index.php
+++ index.php
@@ -11,6 +11,26 @@
         <meta property="og:title" content="トップページ" />
         <meta property="og:description" content="デレステとミリシタのキャラを知ってるかどうか答えてどっちのプロデューサーか判定するクソサイト" />
         <meta property="og:type" content="website" />
-        ß
+        <?php
+            $game = rand(0, 1);
+            //0 = デレステ, 1 = ミリシタ
+            if ($game == 0) {
+                $idol = rand(1, 188);
+                print('<link rel="shortcut icon" href="idols-deremas/'.$idol.'.png">');
+            } else {
+                $idol = rand(1, 53);
+                print('<link rel="shortcut icon" href="idols-mirisita/'.$idol.'.png">');
+            }
+        ?>
     </head>
+    <body>
+        <center>
+            <h2>アイマス踏み絵</h2>
+            <p>このサイトは、デレステとミリシタのキャラを知ってるかどうか答えて、あなたはどっちのPさんなのかを判定するサイトです。</p>
+            <a href="question.php">問題へ進む</a>
+            <hr />
+            <h4>注意</h4>
+            <p>このサイトはプログラミングのお勉強のために作ったクソサイトです。営利目的ではありません。</p>
+        </center>
+    </body>
 </html>
