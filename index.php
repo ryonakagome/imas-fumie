4239b686cff580d69777773b87e121380e6889f3
Ryo Nakagome <nakagome@tento-net.com>
2018-03-20 00:24:01 +0900

    デレステの登場人物の画像を追加

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+
+        <meta name="author" content="Naayo" />
+        <meta name="description" content="デレステとミリシタのキャラを知ってるかどうか答えてどっちのプロデューサーか判定するクソサイト" />
+        <title>アイマス踏み絵</title>
+
+        <meta property="og:site_name" content="アイマス踏み絵" />
+        <meta property="og:title" content="トップページ" />
+        <meta property="og:description" content="デレステとミリシタのキャラを知ってるかどうか答えてどっちのプロデューサーか判定するクソサイト" />
+        <meta property="og:type" content="website" />
+        ß
+    </head>
+</html>
